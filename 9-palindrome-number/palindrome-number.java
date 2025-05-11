f407460136bca5a43e6c421757ccdaaa1ce91060
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-05-11 19:00:09 +0530

    Time: 5 ms (71.15%) | Memory: 43.9 MB (88.05%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        int reversed = 0;
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
+        int n=x;
+        int rev=0;
+        while(x>0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
         }
-        return x == reversed || x == reversed / 10;
+        if(rev==n){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
