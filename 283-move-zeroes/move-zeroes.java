3516344af4341b011aee4714f973e6b421c9a094
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-17 23:13:43 +0530

    Time: 2 ms (81.13%) | Memory: 45.2 MB (99.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,11 +3,9 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                if(i!=j){
-                    int temp=nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                }
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
                 j++;
             }
         }
