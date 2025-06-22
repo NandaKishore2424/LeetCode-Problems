3d1ce3a06fdc97fc85b7746160a33757417c81a4
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-22 20:46:21 +0530

    Time: 4 ms (77.78%) | Memory: 44.6 MB (49.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,15 @@
+import java.util.Arrays;
+
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
-        int[] count = new int[26];
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++; 
-            count[t.charAt(i) - 'a']--; 
-        }
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
 
-        for (int c : count) {
-            if (c != 0) return false;
-        }
-
-        return true;
+        return Arrays.equals(sArr, tArr);
     }
 }
