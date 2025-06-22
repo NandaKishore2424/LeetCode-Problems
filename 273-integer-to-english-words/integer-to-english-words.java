a6eaaa2dbb8e9a5c9a6ecedc98d5564266410446
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-22 20:50:25 +0530

    Time: 11 ms (21.42%) | Memory: 43 MB (15.08%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,27 +1,21 @@
 public class Solution {
-    private final String[] LESS_THAN_20 = {
-        "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-        "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
-        "Eighteen", "Nineteen"
-    };
-    
-    private final String[] TENS = {
-        "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
-    };
-    
-    private final String[] THOUSANDS = {
-        "", "Thousand", "Million", "Billion"
-    };
+    private final String[] below20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+                                      "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                                      "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] thousands = {"", "Thousand", "Million", "Billion"};
 
     public String numberToWords(int num) {
         if (num == 0) return "Zero";
-        
-        StringBuilder result = new StringBuilder();
+
         int i = 0;
+        StringBuilder result = new StringBuilder();
 
         while (num > 0) {
-            if (num % 1000 != 0) {
-                result.insert(0, helper(num % 1000) + THOUSANDS[i] + " ");
+            int chunk = num % 1000;
+            if (chunk != 0) {
+                String chunkWords = helper(chunk).trim() + " " + thousands[i] + " ";
+                result.insert(0, chunkWords);
             }
             num /= 1000;
             i++;
@@ -32,8 +26,8 @@ public class Solution {
 
     private String helper(int num) {
         if (num == 0) return "";
-        else if (num < 20) return LESS_THAN_20[num] + " ";
-        else if (num < 100) return TENS[num / 10] + " " + helper(num % 10);
-        else return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+        else if (num < 20) return below20[num] + " ";
+        else if (num < 100) return tens[num / 10] + " " + helper(num % 10);
+        else return below20[num / 100] + " Hundred " + helper(num % 100);
     }
 }
