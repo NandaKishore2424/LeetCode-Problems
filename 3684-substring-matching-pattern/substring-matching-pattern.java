432d53e8491ad1f8735fc3eb9e799f1ca64cbd8e
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-24 10:14:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (94.04%) - LeetSync

--- 3684-substring-matching-pattern/substring-matching-pattern.java
+++ 3684-substring-matching-pattern/substring-matching-pattern.java
@@ -1,20 +1,11 @@
-public class Solution {
+class Solution {
     public boolean hasMatch(String s, String p) {
-        String[] parts = p.split("\\*", -1);
-        String prefix = parts[0];
-        String suffix = parts[1];
-
-        int minLen = prefix.length() + suffix.length();
-
-        for (int i = 0; i <= s.length() - minLen; i++) {
-            for (int j = i + minLen; j <= s.length(); j++) {
-                String sub = s.substring(i, j);
-                if (sub.startsWith(prefix) && sub.endsWith(suffix)) {
-                    return true;
-                }
-            }
+        int index = p.indexOf('*');
+        int firstpos = s.indexOf(p.substring(0, index)); 
+        int secondpos = s.indexOf(p.substring(index + 1), firstpos + index); 
+        if (firstpos != -1 && secondpos != -1) {
+            return true;
         }
-
         return false;
     }
 }
