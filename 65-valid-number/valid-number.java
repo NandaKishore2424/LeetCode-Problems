a0f75718bbfe000711e4ed82bd5ae640db451ea4
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-24 10:16:09 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (73.89%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -8,20 +8,16 @@ class Solution {
             if (c >= '0' && c <= '9') {
                 num = true;
             } else if (c == 'e' || c == 'E') {
-                if (exp || !num) 
-                return false;// false, if no number before exp
+                if (exp || !num) return false;// false, if no number before exp
                 exp = true;
                 sign = false;
                 num = false;
                 dec = false;
-            } else if (c == '+' || c == '-')
-             {
-                if (sign || num || dec) 
-                return false; // false, any num or dec before
+            } else if (c == '+' || c == '-') {
+                if (sign || num || dec) return false; // false, any num or dec before
                 sign = true;
             } else if (c == '.') {
-                if (dec || exp) 
-                return false; //false if any exp before 
+                if (dec || exp) return false; //false if any exp before 
                 dec = true;
             } else {
                 return false;
