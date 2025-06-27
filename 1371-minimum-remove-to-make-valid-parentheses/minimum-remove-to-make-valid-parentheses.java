b32d868fa4789904d597ed21922098865ba36e9a
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-27 19:34:45 +0530

    Time: 17 ms (78.06%) | Memory: 45.6 MB (59.70%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> st=new Stack<>();
+        StringBuilder str=new StringBuilder(s);
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(')
+            {
+                st.push(i);
+            }
+            else if(ch==')'){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+                else{
+                    str.deleteCharAt(i);
+                    i--;
+                }
+            }
+        }
+        while(!st.isEmpty()){
+            str.deleteCharAt(st.pop());
+        }
+        return str.toString();
+
+    }
+}
