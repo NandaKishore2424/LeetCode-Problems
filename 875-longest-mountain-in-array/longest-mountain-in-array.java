8c38884ee30695bf72b8ac7bd069317a9bbad7de
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-28 10:17:26 +0530

    Time: 3 ms (63.36%) | Memory: 44.9 MB (61.28%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -4,11 +4,10 @@ class Solution {
         if(n<3) return 0;
         int max=0;
         for(int i=1;i<n-1;i++){
-            if(arr[i]> arr[i-1] && arr[i]>arr[i+1])
-            {
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
                 int left=i;
                 int right=i;
-                while(left>0 && arr[left-1] < arr[left]){
+                while(left>0 && arr[left-1]<arr[left]){
                     left--;
                 }
                 while(right<n-1 && arr[right+1]<arr[right]){
@@ -16,7 +15,6 @@ class Solution {
                 }
                 int curr=right-left+1;
                 max=Math.max(max, curr);
-
             }
         }
         return max;
