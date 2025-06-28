64e127f49429deeb2cf95696af5eda961fc50fbb
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-28 10:12:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (20.37%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
         if(n==0) return 1;
-        int total = 10, unique=9, ava=9;
-        for(int i=2;i<=n && ava>0 ; i++){
+        int total=10, unique=9, ava=9;
+        for(int i=2;i<=n && ava>0; i++){
             unique*=ava;
             total+=unique;
             ava--;
