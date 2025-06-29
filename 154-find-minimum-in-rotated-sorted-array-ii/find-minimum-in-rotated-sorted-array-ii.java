06dff28695472489cee14c69f3e1ec5aaa0647d3
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 11:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (14.28%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -9,8 +9,7 @@ class Solution {
             else if(nums[mid]>nums[right]){
                 left=mid+1;
             }
-            else
-            {
+            else{
                 right--;
             }
         }
