596cdf3dc4ca21a5ae334a565f019dad22aa77ba
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 20:10:02 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (86.37%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     public boolean isNumber(String S) {
         boolean num = false, exp = false, sign = false, dec = false;
         
@@ -8,16 +8,16 @@ class Solution {
             if (c >= '0' && c <= '9') {
                 num = true;
             } else if (c == 'e' || c == 'E') {
-                if (exp || !num) return false;// false, if no number before exp
+                if (exp || !num) return false;
                 exp = true;
                 sign = false;
                 num = false;
                 dec = false;
             } else if (c == '+' || c == '-') {
-                if (sign || num || dec) return false; // false, any num or dec before
+                if (sign || num || dec) return false; 
                 sign = true;
             } else if (c == '.') {
-                if (dec || exp) return false; //false if any exp before 
+                if (dec || exp) return false; 
                 dec = true;
             } else {
                 return false;
