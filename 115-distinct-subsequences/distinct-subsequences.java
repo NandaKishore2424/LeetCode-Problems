5dcb94cd567daf0525999db2db665e4460a2e325
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 19:50:33 +0530

    Time: 15 ms (90.32%) | Memory: 41.8 MB (92.52%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int numDistinct(String a, String b) {
-        int m = a.length(), n = b.length();
-        int[][] d = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) d[i][0] = 1;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    d[i][j] = d[i - 1][j - 1] + d[i - 1][j];
-                } else {
-                    d[i][j] = d[i - 1][j];
+    public int numDistinct(String s, String t) {
+        int m=s.length(), n=t.length();
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        for(int i =1;i<=m;i++){
+            for(int j=n; j>=1;j--){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[j]+=dp[j-1];
                 }
             }
         }
-        return d[m][n];
+        return dp[n];
     }
 }
