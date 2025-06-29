af7cef6a7ad349e2417f5fe316c2815ced217806
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 20:18:04 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (73.80%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,25 +1,27 @@
-public class Solution {
+class Solution {
     public boolean isNumber(String S) {
-        boolean num = false, exp = false, sign = false, dec = false;
-        
-        for (int i = 0; i < S.length(); i++) {
-            char c = S.charAt(i);
-            
-            if (c >= '0' && c <= '9') {
-                num = true;
-            } else if (c == 'e' || c == 'E') {
-                if (exp || !num) return false;
-                exp = true;
-                sign = false;
-                num = false;
-                dec = false;
-            } else if (c == '+' || c == '-') {
-                if (sign || num || dec) return false; 
-                sign = true;
-            } else if (c == '.') {
-                if (dec || exp) return false; 
-                dec = true;
-            } else {
+        boolean num=false, dec=false, sign=false, exp=false;
+        for(int i=0;i<S.length();i++){
+            char c=S.charAt(i);
+            if(Character.isDigit(c)){
+                num=true;
+            }
+            else if (c=='e'||c=='E'){
+                if(exp || !num) return false;
+                exp=true;
+                sign=false;
+                dec=false;
+                num=false;
+            }
+            else if(c=='+'||c=='-'){
+                if(sign || num || dec) return false;
+                sign=true;
+            }
+            else if(c=='.'){
+                if(dec || exp) return false;
+                dec=true;
+            }
+            else{
                 return false;
             }
         }
