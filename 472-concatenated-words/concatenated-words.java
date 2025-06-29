2c984a865ba66403129b0dbf0010c6b6de780194
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 16:19:55 +0530

    Time: 76 ms (28.80%) | Memory: 49.4 MB (63.65%) - LeetSync

--- 472-concatenated-words/concatenated-words.java
+++ 472-concatenated-words/concatenated-words.java
@@ -1,33 +1,38 @@
 class Solution {
-    public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> result = new ArrayList<>();
-        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
-        Arrays.sort(words, (a, b) -> a.length() - b.length()); // Start from shortest words
-
-        for (String word : words) {
-            if (word.isEmpty()) continue;
-            wordSet.remove(word); // Avoid using itself
-            if (canFormDP(word, wordSet)) {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) 
+    {
+        List <String> result = new ArrayList<>();
+        Set <String>  wordSet = new HashSet<>(Arrays.asList(words));
+        Arrays.sort(words,(a,b) -> a.length()- b.length());
+        for(String word : words)
+        {
+            if(word.isEmpty()) continue;
+            wordSet.remove(word);
+            if(canFormDp(word,wordSet))
+            {
                 result.add(word);
             }
-            wordSet.add(word);
+           wordSet.add(word);
         }
         return result;
     }
-
-    private boolean canFormDP(String word, Set<String> wordSet) {
-        boolean[] dp = new boolean[word.length() + 1];
-        dp[0] = true;
-
-        for (int i = 1; i <= word.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordSet.contains(word.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+    private boolean canFormDp(String word, Set<String>  wordSet)
+    {
+        boolean[] dp = new boolean[word.length()+1];
+        dp[0]  = true;
+        for(int i=1; i<= word.length();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(dp[j] && wordSet.contains(word.substring(j,i)))
+                {
+                 dp[i] = true;
+                 break;
                 }
             }
         }
         return dp[word.length()];
     }
-}
+    
 
+}
