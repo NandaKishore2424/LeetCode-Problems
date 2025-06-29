40b7bedb019de6085fecddcd8efdada30daa87e5
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-29 22:23:52 +0530

    Time: 68 ms (50.39%) | Memory: 50.3 MB (53.96%) - LeetSync

--- 472-concatenated-words/concatenated-words.java
+++ 472-concatenated-words/concatenated-words.java
@@ -1,31 +1,24 @@
-public class Solution {
+class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> res = new ArrayList<>();
-        Set<String> dict = new HashSet<>(Arrays.asList(words));
-
-        for (String word : words) {
-            if (canForm(word, dict, new HashMap<>())) {
+        List<String> res=new ArrayList<>();
+        Set<String> dict=new HashSet<>(Arrays.asList(words));
+        for(String word:words){
+            if(canForm(word, dict, new HashMap<>())){
                 res.add(word);
             }
         }
-
         return res;
     }
-
-    private boolean canForm(String word, Set<String> dict, Map<String, Boolean> memo) {
-        if (memo.containsKey(word)) return memo.get(word);
-
-        for (int i = 1; i < word.length(); i++) {
-            String prefix = word.substring(0, i);
-            String suffix = word.substring(i);
-
-            if (dict.contains(prefix) && 
-                (dict.contains(suffix) || canForm(suffix, dict, memo))) {
+    private static boolean canForm(String word, Set<String> dict, Map<String, Boolean> memo){
+        if(memo.containsKey(word)) return memo.get(word);
+        for(int i=1;i<word.length();i++){
+            String prefix=word.substring(0,i);
+            String suffix=word.substring(i);
+            if(dict.contains(prefix) && (dict.contains(suffix) || canForm(suffix, dict, memo))){
                 memo.put(word, true);
                 return true;
             }
         }
-
         memo.put(word, false);
         return false;
     }
