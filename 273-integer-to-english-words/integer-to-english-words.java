e62923bec66024b233e139c38a8bbf967890e536
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-06-30 10:32:02 +0530

    Time: 13 ms (8.72%) | Memory: 42.7 MB (31.50%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,45 +1,28 @@
 class Solution {
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+                                           "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                                           "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     
-        final static String[] digits = {
-            "", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ","Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen ", "Twenty "
-        };
-
-        final static String[] tens = {
-            "", "Ten ", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "
-        };
-
-        final static String[] powers = {
-            "Hundred ", "Thousand ", "Million ", "Billion " 
-        };
-
+    private final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
     public String numberToWords(int num) {
-        if(num == 0) return "Zero";
-        StringBuilder word = new StringBuilder();
-        toEng(num, word);
-        return word.toString().trim();
-    }
-    private void toEng(int num, StringBuilder w){
-        if(num <= 20){
-            w.append(digits[num]);
-        }else if(num < 100){
-            w.append(tens[(num - num%10)/10]);
-            w.append(digits[num%10]);
-        }else if(num < 1000){
-            w.append(digits[(num - num%100)/100]);
-            w.append(powers[0]);
-            toEng(num%100, w);
-        }else if(num < 1000_000){
-            toEng((num - num%1000)/1000, w);
-            w.append(powers[1]);
-            toEng(num%1000, w);
-        }else if(num < 1000_000_000){
-            toEng((num - num%1000_000)/1000_000, w);
-            w.append(powers[2]);
-            toEng(num%1000_000, w);
-        }else if(num <= Integer.MAX_VALUE){
-            toEng((num - num%1000_000_000)/1000_000_000, w);
-            w.append(powers[3]);
-            toEng(num%1000_000_000, w);
+        if(num==0) return "Zero";
+        String res="";
+        int i=0;
+        while(num>0){
+            if(num%1000!=0){
+                res=helper(num%1000)+THOUSANDS[i]+" "+res;
+            }
+            num/=1000;
+            i++;
         }
+        return res.trim();
+    }
+    private String helper(int num){
+        if(num==0) return "";
+        else if(num<20) return LESS_THAN_20[num]+" ";
+        else if(num<100) return TENS[num/10]+" "+helper(num%10);
+        else return LESS_THAN_20[num/100]+" Hundred "+helper(num%100);
     }
 }
