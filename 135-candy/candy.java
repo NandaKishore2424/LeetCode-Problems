8fba4766b5ce97930ce73d90b02e822a11c77323
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-07-03 10:00:53 +0530

    Time: 3 ms (74.65%) | Memory: 45.6 MB (79.58%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.fill(candies, 1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                candies[i]=candies[i-1]+1;
+                candies[i]=1+candies[i-1];
             }
         }
         for(int i=n-2;i>=0;i--){
@@ -13,11 +13,10 @@ class Solution {
                 candies[i]=Math.max(candies[i], candies[i+1]+1);
             }
         }
-        int total=0;
+        int tot=0;
         for(int c:candies){
-            total+=c;
+            tot+=c;
         }
-        return total;
-        
+        return tot;
     }
 }
