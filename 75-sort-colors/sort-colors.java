19fba318c3226b7180c4cbb15e04aba231914174
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-07-10 13:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (49.67%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -10,18 +10,15 @@ class Solution {
             else if(nums[mid]==1){
                 mid++;
             }
-            else
-            {
-                swap(nums, high, mid);
-                high--; 
+            else {
+                swap(nums, mid, high);
+                high--;
             }
         }
     }
     private static void swap(int[] nums, int a, int b){
-        if(a!=b){
-            int temp=nums[a];
-            nums[a]=nums[b];
-            nums[b]=temp;
-        }
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
 }
