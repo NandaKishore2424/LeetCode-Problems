b1a66b372034ffd2ebd60c5ee4defc52162cde96
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-07-10 20:10:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (7.87%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -4,18 +4,21 @@ class Solution {
         int n=matrix[0].length;
         boolean firstrowzero=false;
         boolean firstcolzero=false;
+        //first row lets check for the zero
         for(int j=0;j<n;j++){
             if(matrix[0][j]==0){
                 firstrowzero=true;
                 break;
             }
         }
+        //now lets check the col
         for(int i=0;i<m;i++){
             if(matrix[i][0]==0){
                 firstcolzero=true;
                 break;
             }
         }
+        //mark what row and col has zero
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
