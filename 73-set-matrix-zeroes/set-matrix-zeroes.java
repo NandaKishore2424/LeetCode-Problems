bfdc74dfd561270b20d8cd34cafa98cd3f6b7fdb
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-07-10 10:55:00 +0530

    Time: 1 ms (72.41%) | Memory: 45.7 MB (64.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,43 +2,21 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        boolean isFirstRowZero = false;
-        boolean isFirstColumnZero= false;
-        for(int i=0; i<m;i++){
-            if(matrix[i][0]==0){
-                isFirstColumnZero=true;
-                break;
-            }
-        }
-        for(int j=0;j<n;j++){
-            if(matrix[0][j]==0){
-                isFirstRowZero=true;
-                break;
-            }
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+        boolean[] row=new boolean[m];
+        boolean[] col=new boolean[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] || col[j]){
                     matrix[i][j]=0;
                 }
-            }
-        }
-        if(isFirstRowZero){
-            for(int j=0;j<n;j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(isFirstColumnZero){
-            for(int i=0;i<m;i++){
-                matrix[i][0]=0;
             }
         }
     }
