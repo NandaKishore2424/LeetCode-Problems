cd4076d61c1e031f3909810497ed458a1a3d115d
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-10-07 23:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (97.11%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    static{
+        System.gc();
+        for(int i = 0; i < 500; i++){
+            new Solution().canJump(new int[] {2, 3, 1, 1, 4});
+        }
+    }
+    
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        int n = nums.length;
+        int maxReach = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) return true;
+        }
+
+        return maxReach >= n - 1;
+    }
+}
