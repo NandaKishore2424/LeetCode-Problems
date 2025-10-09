95b6a4b17367a271bbe9b67c441a5097815f4074
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-10-09 22:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (15.13%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,17 +1,41 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
-        reverse(nums, 0, nums.length-1);
-        reverse(nums, 0,k-1);
-        reverse(nums, k, nums.length-1);
+        nanda(nums, 0, nums.length-1); //- step 1 rotate whole array -- 0, n-1
+        nanda(nums, 0, k-1);           //- step 2 rotate first k elements - 0, k-1
+        nanda(nums, k, nums.length-1); //- step 3 rotate remaining  k, n-1
     }
-    private void reverse(int[] nums, int start, int end){
-        while(start<end){
+    private void nanda(int[] nums, int start, int last){
+        while(start<last){
             int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+            nums[start]=nums[last];
+            nums[last]=temp;
             start++;
-            end--;
+            last--;
         }
     }
 }
+/*n=7 n-1=6
+k-1 = 2
+1234567 - arr
+0123456 - index
+
+7654321 - step1
+5674321 - sep2
+5671234 step3 
+
+
+1 2 3 4 5 6 7 - ip
+k = 3
+
+step 1 - reverse whole array - 7 6 5 4 3 2 1 
+step 2- reverse first k number - 5 6 7 4 3 2 1
+step 3 - reverse remaining - 5 6 7 1 2 3 4 
+
+5 6 7 1 2 3 4  - op
+
+array len = 7
+k = 12
+k=k%7 = 12% 7 = 5 = k
+step 1: reverse 
+*/
