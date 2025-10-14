21bf7ab75dc6e93764819bda3b65a25126a48dc8
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-10-14 08:58:36 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (28.23%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,48 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        
-        // Ensure nums1 is the smaller array for simplicity
-        if (n1 > n2)
+        if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
-        
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
-        int low = 0, high = n1;
-        
-        while (low <= high) {
-            int mid1 = (low + high) >> 1; // Calculate mid index for nums1
-            int mid2 = left - mid1; // Calculate mid index for nums2
-            
-            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
-            
-            // Determine values of l1, l2, r1, and r2
-            if (mid1 < n1)
-                r1 = nums1[mid1];
-            if (mid2 < n2)
-                r2 = nums2[mid2];
-            if (mid1 - 1 >= 0)
-                l1 = nums1[mid1 - 1];
-            if (mid2 - 1 >= 0)
-                l2 = nums2[mid2 - 1];
-            
-            if (l1 <= r2 && l2 <= r1) {
-                // The partition is correct, we found the median
-                if (n % 2 == 1)
-                    return Math.max(l1, l2);
-                else
-                    return ((double)(Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
-            }
-            else if (l1 > r2) {
-                // Move towards the left side of nums1
-                high = mid1 - 1;
-            }
-            else {
-                // Move towards the right side of nums1
-                low = mid1 + 1;
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int totalLeft = (m + n + 1) / 2;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+            int i = (left + right) / 2;
+            int j = totalLeft - i;
+
+            int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+            int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
+            int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+            int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
+
+            if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
+                
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(nums1LeftMax, nums2LeftMax) + 
+                            Math.min(nums1RightMin, nums2RightMin)) / 2.0;
+                } else {
+                    return Math.max(nums1LeftMax, nums2LeftMax);
+                }
+            } else if (nums1LeftMax > nums2RightMin) {
+                right = i - 1; 
+            } else {
+                left = i + 1; 
             }
         }
-        
-        return 0; // If the code reaches here, the input arrays were not sorted.
+
+        throw new IllegalArgumentException("Input arrays are not sorted.");
     }
 }
