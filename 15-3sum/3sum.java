71f49a9dd12b47e81f8af78f357954af9757df2c
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-10-15 19:08:33 +0530

    Time: 30 ms (71.21%) | Memory: 51.4 MB (70.19%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -6,7 +6,7 @@ public class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for i
+
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             int left = i + 1;
