fbd3effc6c058bd7f7cf435f76c50a25ee7673b6
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-12-01 15:08:54 +0530

    Time: 1 ms (100.00%) | Memory: 59.1 MB (33.75%) - LeetSync

--- 335-self-crossing/self-crossing.java
+++ 335-self-crossing/self-crossing.java
@@ -1,49 +1,25 @@
-public class Solution {
+class Solution {
     public boolean isSelfCrossing(int[] x) {
-        int a1, a2, a3, a4, a5;
-        
-        // if it's increasing
-        boolean up = false;
-        
-        if (x.length < 4) {
+        if(x.length <= 3){
             return false;
         }
-        
-        a1 = 0;
-        a2 = x[0];
-        a3 = x[1];
-        a4 = x[2];
-        
-        if (a2 < a4) {
-            up = true;
+        int i=2;
+        while(i < x.length && x[i] > x[i-2]){
+            i++;
         }
-        else {
-            up = false;
+        if(i >= x.length){
+            return false;
         }
-        
-        for (int i = 3; i < x.length; i++) {
-            a5 = x[i];
-            
-            if (!up && a5 >= a3) {
+        if((i >=4 && x[i] >= x[i-2] - x[i-4]) ||(i ==3 &&x[i] == x[i-2])){
+            x[i -1] -= x[i - 3];
+        }
+        i++;
+        while(i< x.length){
+            if(x[i] >= x[i-2]){
                 return true;
             }
-            else if (up && a5 <= a3) {
-                // succeeded in turning into decreasing
-                if (a5 + a1 < a3 || (i + 1 < x.length && x[i + 1] + a2 < a4)) {
-                    up = false;
-                }
-                // not end yet
-                else if (i + 1 < x.length) {
-                    return true;
-                }
-            }
-            
-            a1 = a2;
-            a2 = a3;
-            a3 = a4;
-            a4 = a5;
+            i++;
         }
-        
         return false;
     }
 }
