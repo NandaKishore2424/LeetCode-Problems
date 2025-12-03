0c151c7c304982e0cb55efd337850fe927efceac
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2025-12-03 20:47:01 +0530

    Time: 385 ms (96.09%) | Memory: 274.8 MB (71.10%) - LeetSync

--- 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.java
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.java
@@ -1,64 +1,76 @@
 class Solution {
+
     public int countTrapezoids(int[][] points) {
         int n = points.length;
-        
-        HashMap<Integer, HashMap<Integer, Integer>> segments = new HashMap<>();
-        HashMap<Integer, HashMap<Integer, Integer>> parallel_segments = new HashMap<>();
+        double inf = 1e9 + 7;
+        Map<Double, List<Double>> slopeToIntercept = new HashMap<>();
+        Map<Integer, List<Double>> midToSlope = new HashMap<>();
+        int ans = 0;
 
         for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
             for (int j = i + 1; j < n; j++) {
-                int dx = points[j][0] - points[i][0];
-                int dy = points[j][1] - points[i][1];
-                
-                if (dx < 0 || (dx == 0 && dy < 0)) {
-                    dx = -dx;
-                    dy = -dy;
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int dx = x1 - x2;
+                int dy = y1 - y2;
+                double k;
+                double b;
+
+                if (x2 == x1) {
+                    k = inf;
+                    b = x1;
+                } else {
+                    k = (1.0 * (y2 - y1)) / (x2 - x1);
+                    b = (1.0 * (y1 * dx - x1 * dy)) / dx;
                 }
-                
-                int g = gcd(dx, Math.abs(dy));
-                int sx = dx / g;
-                int sy = dy / g;
-                
-                int line_param = sx * points[i][1] - sy * points[i][0];
-                
-                int key_normalized = (sx << 12) | (sy + 2000);
-                int key_full = (dx << 12) | (dy + 2000);
-                
-                segments.computeIfAbsent(key_normalized, k -> new HashMap<>())
-                       .merge(line_param, 1, Integer::sum);
-                parallel_segments.computeIfAbsent(key_full, k -> new HashMap<>())
-                                .merge(line_param, 1, Integer::sum);
+                if (k == -0.0) {
+                    k = 0.0;
+                }
+                if (b == -0.0) {
+                    b = 0.0;
+                }
+                int mid = (x1 + x2) * 10000 + (y1 + y2);
+                slopeToIntercept
+                    .computeIfAbsent(k, key -> new ArrayList<>())
+                    .add(b);
+                midToSlope
+                    .computeIfAbsent(mid, key -> new ArrayList<>())
+                    .add(k);
             }
         }
-        
-        return calculate(segments) - calculate(parallel_segments) / 2;
-    }
-    
-    private int calculate(HashMap<Integer, HashMap<Integer, Integer>> map) {
-        long result = 0;
-        
-        for (HashMap<Integer, Integer> inner_map : map.values()) {
-            long total = 0;
-            
-            for (int count : inner_map.values()) {
-                total += count;
+
+        for (List<Double> sti : slopeToIntercept.values()) {
+            if (sti.size() == 1) {
+                continue;
             }
-            
-            for (int count : inner_map.values()) {
-                total -= count;
-                result += (long) count * total;
+            Map<Double, Integer> cnt = new TreeMap<>();
+            for (double b : sti) {
+                cnt.put(b, cnt.getOrDefault(b, 0) + 1);
+            }
+            int sum = 0;
+            for (int count : cnt.values()) {
+                ans += sum * count;
+                sum += count;
             }
         }
-        
-        return (int) result;
-    }
-    
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
+
+        for (List<Double> mts : midToSlope.values()) {
+            if (mts.size() == 1) {
+                continue;
+            }
+            Map<Double, Integer> cnt = new TreeMap<>();
+            for (double k : mts) {
+                cnt.put(k, cnt.getOrDefault(k, 0) + 1);
+            }
+            int sum = 0;
+            for (int count : cnt.values()) {
+                ans -= sum * count;
+                sum += count;
+            }
         }
-        return Math.abs(a);
+
+        return ans;
     }
 }
