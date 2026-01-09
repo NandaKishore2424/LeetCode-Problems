66f201cb69bfb4196267fc58566394830707dcea
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2026-01-09 11:31:29 +0530

    Time: 5 ms (57.66%) | Memory: 44.6 MB (64.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,10 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()-1 != t.length()-1) return false;
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        if(s.length()!=t.length()){
+            return false;   
+        }
+        int[] charCounts=new int[26];
+        for(int i=0;i<s.length();i++){
+            charCounts[s.charAt(i)-'a']++;
+            charCounts[t.charAt(i)-'a']--;
+        }
+        for(int count:charCounts){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
