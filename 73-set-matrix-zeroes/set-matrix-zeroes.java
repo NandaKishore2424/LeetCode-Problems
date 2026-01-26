d501f7e3c82dd7ee19619930844a1ea9d7b1668d
Nanda Kishore <nandakishorer.student@saveetha.ac.in>
2026-01-26 16:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (80.20%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,47 +1,44 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+    public void setZeroes(int[][] nums) {
         boolean firstrowzero=false;
         boolean firstcolzero=false;
-        //first row lets check for the zero
-        for(int j=0;j<n;j++){
-            if(matrix[0][j]==0){
+        int m=nums.length;
+        int n=nums[0].length;
+        for(int i=0;i<n;i++){
+            if(nums[0][i]==0){
                 firstrowzero=true;
                 break;
             }
         }
-        //now lets check the col
         for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
+            if(nums[i][0]==0){
                 firstcolzero=true;
                 break;
             }
         }
-        //mark what row and col has zero
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
+                if(nums[i][j]==0){
+                    nums[i][0]=0;
+                    nums[0][j]=0;
                 }
             }
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
-                    matrix[i][j]=0;
+                if(nums[i][0] == 0 || nums[0][j]==0){
+                    nums[i][j]=0;
                 }
             }
         }
         if(firstrowzero){
-            for(int j=0;j<n;j++){
-                matrix[0][j]=0;
+            for(int i=0;i<n;i++){
+                nums[0][i]=0;
             }
         }
         if(firstcolzero){
             for(int i=0;i<m;i++){
-                matrix[i][0]=0;
+                nums[i][0]=0;
             }
         }
     }
